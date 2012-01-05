cb507e643999946f93e4decf113d014ea1a2f4f4
Sebastian Riedel <sebastian.riedel@gmail.com>
2012-01-04 21:14:11 -0500

    all submodules now are sbt-ed.

--- project/ThePPLBuild.scala
+++ project/ThePPLBuild.scala
@@ -2,15 +2,16 @@ import sbt._
 import Keys._
 
 object ThePPLBuild extends Build {
-    lazy val root = Project(id = "theppl",
-                            base = file(".")) aggregate(core)
+  lazy val root = Project(id = "theppl",
+    base = file(".")) aggregate(core, datasets, examples)
 
+  lazy val core = Project(id = "theppl-core",
+    base = file("theppl-core"))
 
-    lazy val core = Project(id = "theppl-core",
-                           base = file("theppl-core"))
+  lazy val datasets = Project(id = "theppl-datasets",
+    base = file("theppl-datasets"))
 
-/*  
-  lazy val bar = Project(id = "hello-bar",
-                           base = file("bar"))
-*/
+  lazy val examples = Project(id = "theppl-examples",
+    base = file("theppl-examples")) dependsOn(core)
+
 }
---
+++ theppl-datasets/build.sbt
@@ -0,0 +1,2 @@
+name := "theppl-datasets"
+
