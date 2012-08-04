ae6ec027a3b2da220f6abd6336b4649d9ea38159
Sebastian Riedel <sebastian.riedel@gmail.com>
2012-08-04 19:35:30 +0100

    fixed a small bug in progress monitor that omitted last step of last round

--- theppl-core/src/main/scala/com/github/riedelcastro/theppl/util/ProgressMonitor.scala
+++ theppl-core/src/main/scala/com/github/riedelcastro/theppl/util/ProgressMonitor.scala
@@ -9,13 +9,13 @@ import collection.mutable
  */
 class ProgressMonitor {
 
-  def progressString(fractionDone:Double, ticks:Int = 20) = {
+  def progressString(fractionDone: Double, ticks: Int = 20) = {
     val ticksDone = (fractionDone * ticks).toInt
     val format = "[%-" + ticks + "s]%7.2f%%"
-    format.format(Range(0,ticksDone).view.map(i => "=").mkString, fractionDone * 100)
+    format.format(Range(0, ticksDone).view.map(i => "=").mkString, fractionDone * 100)
   }
 
-  class Process(val id:Any, val name:String) {
+  class Process(val id: Any, val name: String) {
     var stepsMax = 0
     var stepsCurrent = 0
     var roundsMax = 1
@@ -23,29 +23,22 @@ class ProgressMonitor {
     var timeStarted = System.currentTimeMillis()
 
     def printStart() {
-      println("Starting %s with %d rounds and %d steps for each".format(name,roundsMax,stepsMax))
-//      print("123")
-//      print("\u0008")
-//      for (i <- 0 until 1000000000; if (i % 10000000 == 0))
-//        print(i + "\r")
-//      println("")
+      println("Starting %s with %d rounds and %d steps for each".format(name, roundsMax, stepsMax))
     }
 
     def printProgress() {
       val fineInterval = math.max(stepsMax / 100, 1)
       if (stepsCurrent % fineInterval == 0 || stepsCurrent == stepsMax)
-        print("\r" + progressString(stepsCurrent / stepsMax.toDouble, 40))
-        //print(".")
-//      if (stepsCurrent % coarseInterval == 0) print(" ")
-
+        print("\r" + progressString((stepsCurrent) / stepsMax.toDouble, 40) +
+          "%3d/%-3d".format(roundsCurrent + 1, roundsMax))
     }
 
   }
 
-  val processes:mutable.ConcurrentMap[Any,Process]= new ConcurrentHashMap[Any, Process]
+  val processes: mutable.ConcurrentMap[Any, Process] = new ConcurrentHashMap[Any, Process]
 
-  def start(id:Any='default,name:String="default",stepsMax:Int,roundsMax:Int = 1) {
-    val process = new Process(id,name)
+  def start(id: Any = 'default, name: String = "default", stepsMax: Int, roundsMax: Int = 1) {
+    val process = new Process(id, name)
     process.stepsCurrent = 0
     process.stepsMax = stepsMax
     process.roundsMax = roundsMax
@@ -53,13 +46,13 @@ class ProgressMonitor {
     process.printStart()
   }
 
-  def progress(id:Any='default, howmuch:Int = 1) {
+  def progress(id: Any = 'default, howmuch: Int = 1) {
     val process = processes(id)
     process.stepsCurrent += howmuch
     if (process.stepsCurrent > process.stepsMax) {
       print("\n")
       process.roundsCurrent += 1
-      process.stepsCurrent = 0
+      process.stepsCurrent = 1
     }
     process.printProgress()
     if (process.roundsCurrent > process.roundsMax) {
