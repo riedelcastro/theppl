8545503b822daf5d5fc90110ee2beff10c920f2b
visenger <visenger@gmail.com>
2013-07-10 20:55:23 +0200

    fixed: type cast error.

--- theppl-core/src/test/scala/com/github/riedelcastro/theppl/parse/MLNTranslatorTest.scala
+++ theppl-core/src/test/scala/com/github/riedelcastro/theppl/parse/MLNTranslatorTest.scala
@@ -8,13 +8,17 @@ import org.scalatest.matchers.MustMatchers
  */
 class MLNTranslatorTest extends FunSpec with MustMatchers {
   describe("MLN translator") {
-    ignore("loads MLN specification") {
+    it("loads MLN specification") {
       val mln_file = "theppl-core/src/test/data/mln/social-network/smoking.mln"
       val MLN = new MLNEmbeddedTranslator
       MLN.translateMLNFromFile(mln_file)
       val atoms = MLN.atoms
 
       atoms.foreach(x => println(x))
+
+      val formulae= MLN.formulae
+
+      formulae.foreach(x => println(x))
     }
   }
 
